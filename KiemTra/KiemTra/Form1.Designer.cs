1aa078ac0bd0e36d6be6487e9edb242c37c2eb7d
NAMPPP <87806981+NAMPPP@users.noreply.github.com>
2022-03-11 15:30:21 +0700

    login

--- KiemTra/KiemTra/Form1.Designer.cs
+++ KiemTra/KiemTra/Form1.Designer.cs
@@ -39,24 +39,26 @@ namespace KiemTra
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 75);
+            this.label1.Location = new System.Drawing.Point(91, 92);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(100, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "tên đăng nhập";
             // 
             // textboxusername
             // 
-            this.textboxusername.Location = new System.Drawing.Point(248, 82);
-            this.textboxusername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textboxusername.Location = new System.Drawing.Point(331, 101);
+            this.textboxusername.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.textboxusername.Name = "textboxusername";
-            this.textboxusername.Size = new System.Drawing.Size(357, 22);
+            this.textboxusername.Size = new System.Drawing.Size(475, 22);
             this.textboxusername.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(93, 139);
+            this.label2.Location = new System.Drawing.Point(124, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 2;
@@ -64,32 +66,34 @@ namespace KiemTra
             // 
             // textboxmatkhau
             // 
-            this.textboxmatkhau.Location = new System.Drawing.Point(248, 139);
-            this.textboxmatkhau.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textboxmatkhau.Location = new System.Drawing.Point(331, 171);
+            this.textboxmatkhau.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.textboxmatkhau.Name = "textboxmatkhau";
-            this.textboxmatkhau.Size = new System.Drawing.Size(357, 22);
+            this.textboxmatkhau.Size = new System.Drawing.Size(475, 22);
             this.textboxmatkhau.TabIndex = 3;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(325, 228);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogin.Location = new System.Drawing.Point(433, 281);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(100, 28);
+            this.btnLogin.Size = new System.Drawing.Size(133, 34);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Đăng Nhập";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1423, 682);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.textboxmatkhau);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textboxusername);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- KiemTra/KiemTra/Form1.cs
+++ KiemTra/KiemTra/Form1.cs
@@ -16,5 +16,16 @@ namespace KiemTra
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String username = textboxusername.Text;
+            String password = textboxmatkhau.Text;
+            if (username == "admin" && password == "admin")
+            {
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+        }
     }
 }
